c9486e81f84ea2255e2491ade879fe48b3fc2b33
Tronconneur <thomas.victoria@hetic.net>
2015-04-13 21:27:19 +0200

    Graphik Moise
    Les info sur le nombre de saisons/épisode sont transmit en tableau.
    Les valeur des votes sont désormais accèssible sous formes de tableaux

--- result_page/result.php
+++ result_page/result.php
@@ -5,14 +5,14 @@
 $ch = curl_init();
 
 $key = '?api_key=d0a923b609a899bbb5a493dc98fe31bd';
-$key_series = '61889';
+$key_series = '1402';
 
 curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/changes".$key);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  "Accept: application/json"
+    "Accept: application/json"
 ));
 
 $response = curl_exec($ch);
@@ -30,7 +30,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  "Accept: application/json"
+    "Accept: application/json"
 ));
 
 $response = curl_exec($ch);
@@ -42,8 +42,25 @@ $ct = 0;
 
 $counter = $number_episodes->seasons;
 
+// Moyenne note saison
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/season/0".$key);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Accept: application/json"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$note_saison = json_decode($response);
+
 //    echo '<pre>';
-//    print_r($counter);
+//    print_r($note_saison);
 //    echo '</pre>';
 
 ?>
@@ -51,21 +68,48 @@ $counter = $number_episodes->seasons;
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
-    <script>
-        
-        
-    <?php foreach($counter as $_episodes): 
-        $ct = $ct+1;
-        $episode =$ct-1;?>
-        var nombre_episode_saison<?=$ct?> = <?= $counter[$episode]->episode_count ?>;
-    <?php endforeach; ?>
-        var nombre_saison =<?= $ct?>;
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
 
-    </script>
-</body>
+        <script>
+            // Génaration tableau nombre de saison = nombre de valeur / nombre d'épisode dans la saison la valeur.
+            var saison_episode = [
+                <?php foreach($counter as $_episodes): 
+                    $ct = $ct+1;
+                    $episode =$ct-1;?>
+                <?= $counter[$episode]->episode_count ?>,
+                <?php endforeach; ?>
+            ];
+            //Génération un tableau par saison des notes des épisodes
+            <?php
+
+                for($x=0; $x < $ct ; $x++){
+
+                    $ch = curl_init();
+
+                    curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/season/".$x.$key);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+                    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                            "Accept: application/json"
+                    ));
+
+                    $response = curl_exec($ch);
+                    curl_close($ch);
+                    $average_seasons = json_decode($response);
+            ?>
+
+            var average_saison<?=$x?> = [
+                <?php foreach($average_seasons->episodes as $episode_average):?>
+                <?= $episode_average->vote_average?>,
+                <?php endforeach; ?>
+            ];
+
+            <?php } ?>
+        </script>
+    </body>
 </html>

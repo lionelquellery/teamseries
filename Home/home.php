de2f79c65eebaf2e346a07eea7f297e144a902a1
Tronconneur <thomas.victoria@hetic.net>
2015-04-13 23:03:38 +0200

    Ajout fonctionnalité
    Ajout d'un champ de recherche sur la Home page, avec sugestion si
    l'utilisateur ne rentre pas un nom de série valide

--- Home/home.php
+++ Home/home.php
@@ -1,33 +1,41 @@
 <?php
 
-    require ('index.php');
+// Clef d'accès au API
+$key = '?api_key=d0a923b609a899bbb5a493dc98fe31bd';
+// Curl accès aux Infos de la séries
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/popular".$key);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
-?>
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Accept: application/json"
+));
 
+$response = curl_exec($ch);
+curl_close($ch);
+// Conversion de Json en PHP
+$data = json_decode($response); 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    
-    <?php for($i = 0; $i < 19; $i++){?>
-        <div>
-            <?= $data->results[$i]->original_name?> 
-        </div>
-        
-        <div>
-            <img src="http://image.tmdb.org/t/p/w300<?= $data->results[$i]->backdrop_path?>" alt="">
-        </div>
-        
-        <div>
-            <?= $i ?>
-        </div>
-        
-        
-    <?php } ?>
-    
-</body>
-</html>
+// Fnction de cherche
+$ch = curl_init();
+
+if(!empty($_POST['search'])){
+    $key_search = '&api_key=d0a923b609a899bbb5a493dc98fe31bd';
+    //chaine de caractère de l'utilisateur
+    $research = $_POST['search'];
+
+curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/search/tv?query=".$research.$key_search);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Accept: application/json"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$search = json_decode($response);
+
+}
--- result_page/result.php
+++ result_page/result.php
@@ -1,63 +1,8 @@
 <?php
 
-//  NOMBRE DE SAISON
+    require('config.php');
 
-$ch = curl_init();
-
-$key = '?api_key=d0a923b609a899bbb5a493dc98fe31bd';
-$key_series = '1402';
-
-curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/changes".$key);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Accept: application/json"
-));
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-$number_saison = json_decode($response);
-
-
-//  NOMBRE D'EPISODE PAR SAISON
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series.$key);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    "Accept: application/json"
-));
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-$number_episodes = json_decode($response);
-$counter = $number_episodes->seasons;
-
-// PERSONNAGES
-
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/credits".$key);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  "Accept: application/json"
-));
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-$characters = json_decode($response);
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -66,6 +11,7 @@ $characters = json_decode($response);
     </head>
     <body>
 
+       <a href="../Home/index.php">Retour a la recherche</a>
         <script>
             // Génaration tableau nombre de saison = nombre de valeur / nombre d'épisode dans la saison la valeur.
             var saison_episode = [
@@ -103,11 +49,7 @@ $characters = json_decode($response);
                 <?php endforeach; ?>
             ];
 
-            <?php } 
-            
-            
-               
-            ?>            
+            <?php } ?>            
         </script>
         
 <!--        Generation chaque personnages série + image de l'acteur-->

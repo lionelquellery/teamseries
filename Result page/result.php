4ceae50b7e7d8ddd0ab53509e99816c76719db74
Tronconneur <thomas.victoria@hetic.net>
2015-04-13 17:21:37 +0200

    Test graph
    Le code pour que Moise puisse intégrer les variables des séries

---
+++ Result page/result.php
@@ -0,0 +1,71 @@
+<?php
+
+//  NOMBRE DE SAISON
+
+$ch = curl_init();
+
+$key = '?api_key=d0a923b609a899bbb5a493dc98fe31bd';
+$key_series = '61889';
+
+curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/changes".$key);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Accept: application/json"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$number_saison = json_decode($response);
+
+
+//  NOMBRE D'EPISODE PAR SAISON
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series.$key);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  "Accept: application/json"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$number_episodes = json_decode($response);
+
+$ct = 0;
+
+$counter = $number_episodes->seasons;
+
+//    echo '<pre>';
+//    print_r($counter);
+//    echo '</pre>';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <script>
+        
+        
+    <?php foreach($counter as $_episodes): 
+        $ct = $ct+1;
+        $episode =$ct-1;?>
+        var nombre_episode_saison<?=$ct?> = <?= $counter[$episode]->episode_count ?>;
+    <?php endforeach; ?>
+        var nombre_saison =<?= $ct?>;
+
+    </script>
+</body>
+</html>

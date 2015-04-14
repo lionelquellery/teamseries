283899af23ca3d427a55e6619eee85c4652f87b5
Tronconneur <thomas.victoria@hetic.net>
2015-04-14 14:25:26 +0200

    Changement sortie data
    change le format de sortie des data et permet d'y accèder par une
    requete ajax

--- raw_php/result_page/result.php
+++ raw_php/result_page/result.php
@@ -12,54 +12,18 @@
     <body>
 
        <a href="../Home/index.php">Retour a la recherche</a>
-        <script>
-            // Génaration tableau nombre de saison = nombre de valeur / nombre d'épisode dans la saison la valeur.
-            var saison_episode = [
-                <?php
-                    $ct = 0;
-                foreach($counter as $_episodes): 
-                    $ct = $ct+1;
-                    $episode =$ct-1;?>
-                <?= $counter[$episode]->episode_count ?>,
-                <?php endforeach; ?>
-            ];
-            //Génération un tableau par saison des notes des épisodes
-            <?php
-
-                for($x=0; $x < $ct ; $x++){
-
-                    $ch = curl_init();
-
-                    curl_setopt($ch, CURLOPT_URL, "http://api.themoviedb.org/3/tv/".$key_series."/season/".$x.$key);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-                    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                            "Accept: application/json"
-                    ));
-
-                    $response = curl_exec($ch);
-                    curl_close($ch);
-                    $average_seasons = json_decode($response);
-            ?>
-
-            var average_saison<?=$x?> = [
-                <?php foreach($average_seasons->episodes as $episode_average):?>
-                <?= $episode_average->vote_average?>,
-                <?php endforeach; ?>
-            ];
-
-            <?php } ?>            
-        </script>
         
 <!--        Generation chaque personnages série + image de l'acteur-->
         <?php foreach($characters->cast as $_character): ?>
             <div>
                 <img src="http://image.tmdb.org/t/p/w300<?= $_character->profile_path ?>" alt="">
             </div>
-            <div>
-                <?= $_character->character?>
-            </div>
+            <span>
+                Personnage :<?= $_character->character?>
+            </span>
+            <span>
+                Acteur : <?= $_character->name?>
+            </span>
         <?php endforeach; ?>
         
     </body>

876428605a904e4c5e5bc966e04632a38d445ba4
louispie <louis.piechowiak@hetic.net>
2015-04-15 10:21:28 +0200

    home Louis

---
+++ louis/home.php
@@ -0,0 +1,39 @@
+<?php
+
+    require ('config/config.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>DATASERIES</title>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/home.css">
+        <link href='http://fonts.googleapis.com/css?family=Raleway:700,900,400,200' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <section>
+            <img class="logo" src="logo.svg" alt="">
+            <form action="#" method="post">
+                <input type="text" class="inputstyle" placeholder="Recherche ta série préférée...">
+            </form>
+        </section>
+
+        <div class="container">
+            <div class="trait"></div>
+            <p class="popular">Les plus consultés :</p>
+            <?php for($i = 0; $i < 19; $i++){?>    
+            <div class="media"> 
+                <img class="media__image" src="http://image.tmdb.org/t/p/w600<?= $data->results[$i]->backdrop_path?>" alt="" />
+                <a href="#"><div class="media__body">
+                    <h2><?= $data->results[$i]->original_name?></h2>
+                </div></a>
+            </div>
+                
+            <?php } ?>
+        </div>
+    </body>
+</html>

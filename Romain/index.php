8373f3043a569f32f058749c15afb333cbde1d68
RomainLapi <lapi.romain@gmail.com>
2015-04-15 15:10:24 +0200

    intégration page dataviz
    yolo

---
+++ Romain/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dataseries</title>
+	<meta description="">
+	<link rel="stylesheet" href="reset.css">
+	<link rel="stylesheet" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="font/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="main">
+		<section class="left">
+			<header>
+				<div class="logo">Data<span class="logo-bold">Series</span></div>
+                <form action="#">
+                	<input type="search" class="inputstyle" placeholder="Search a serie">
+					<button class="search"><i class="fa fa-search"></i></button>
+				</form>
+			</header>
+			<h1>Game of thrones</h1> 
+			<p class="synopsis">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim possimus, sapiente quis obcaecati magnam alias maiores vero eius, dolorum accusantium modi expedita aut! Maiores adipisci totam, placeat error deleniti dolor. Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+			<h2>Popularity over the seasons</h2>
+			<nav>
+				<ul class="seasons">
+					<li><a href="" class="active">All</a></li>
+					<li><a href="">Season 1</a></li>
+					<li><a href="">Season 2</a></li>
+					<li><a href="">Season 3</a></li>
+					<li><a href="">Season 4</a></li>
+
+				</ul>
+			</nav>
+		</section>
+	</div>
+	<div class="right">
+		<img src="img/img1.jpg" alt="#" class="img-serie">
+	</div>
+	
+</body>
+</html>
